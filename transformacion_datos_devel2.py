96e0c2febc0d3e2fc4033f713ee89cadd07b657c
Raul Silva (Kaggle) <raul.silvav@pucp.pe>
2025-09-29 00:34:06 +0000

    feat(transformacion) : Se implemento la logica que el negocio proponia (YYY_Reu_CEO_20250922.doc)

---
+++ transformacion_datos_devel2.py
@@ -0,0 +1,9 @@
+#Este trabajo lo estoy realizando desde un notebook de Kaggle
+import pandas as pd
+# muchas mas lineas de codigo
+# muchas mas lineas de codigo
+# muchas mas lineas de codigo
+# muchas mas lineas de codigo
+# muchas mas lineas de codigo
+# muchas mas lineas de codigo
+# muchas mas lineas de codigo
